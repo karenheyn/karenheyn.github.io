f69f361ed699f65467f0ce24f4707536d5544b81
karenheyn <karenheyn@gmail.com>
2020-01-01 20:47:52 -0500

    deploying test version

--- src/about/about.js
+++ src/about/about.js
@@ -6,7 +6,7 @@ function Projects() {
   return (
     <div className='paper'>
       <h1>Hello world, I'm Karen</h1>
-      <p>I am a software developer </p>
+      <p>I am a software developer</p>
       <h2>
         view and download my resumé
         <a href={resume} target='_blank' rel='noopener noreferrer'>
